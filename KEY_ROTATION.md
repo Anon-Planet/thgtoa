4207d30feea966ecfb15b27eaba10ae71c9a89c8
Alex Anderson <theheadlessserpentsec@protonmail.com>
2022-05-06 05:43:07 +0000

    Rotated signing keys

---
+++ KEY_ROTATION.md
@@ -0,0 +1,23 @@
+PGP/GPG key ID 7DFFD7471FB76E2A8ABBBCDDD769B3749E933B8A is no longer active
+
+Use 42FF35DB9DE7C088AB0FD4A70C216A52F6DF4920 instead
+------------------------------------------------------------------------------------
+Minisign key
+```
+untrusted comment: minisign public key 902835EC74825934
+RWQ0WYJ07DUokK8V/6LNJ9bf/O/QM9k4FSlDmzgEeXm7lEpw3ecYjXDM
+```
+is no longer active
+
+Use
+```
+untrusted comment: minisign public key FE6A09A3AF18F7A7
+RWSn9xivowlq/ihAzclDBxhCxbYz4bLkC8E645lHgSUlQNlDvoTxO5Fv
+```
+instead
+
+Files signed using this key pair can be verified with the following command:
+
+```
+minisign -Vm <file> -P RWSn9xivowlq/ihAzclDBxhCxbYz4bLkC8E645lHgSUlQNlDvoTxO5Fv
+```
--- make.sh
+++ make.sh
@@ -15,11 +15,14 @@ if [[ "$1" == "" ]]; then
 	for f in ./*; do
 		echo "Signing: $f"
 		# verify with GPG
-		gpg --armor --sign "$f"
+		gpg --armor --detach-sign --sign "$f"
 		# verify with `minisign -Vm <file> -P RWQ0WYJ07DUokK8V/6LNJ9bf/O/QM9k4FSlDmzgEeXm7lEpw3ecYjXDM`
 		yes '' | minisign -S -s /home/user/.minisign/minisign.key -m "$f"
 	done
 	cd ../
+	cp /home/user/KEY_ROTATION.md.7DFFD7471FB76E2A8ABBBCDDD769B3749E933B8A.asc ./KEY_ROTATION.md.asc
+	cp /home/user/KEY_ROTATION.md.902835EC74825934.minisig ./KEY_ROTATION.md.minisig
+	gpg --armor --export 42FF35DB9DE7C088AB0FD4A70C216A52F6DF4920 > 42FF35DB9DE7C088AB0FD4A70C216A52F6DF4920.asc
 	sha256sum *.md > sha256sum.txt
 	gpg --armor --sign sha256sum.txt
 	yes '' | minisign -S -s /home/user/.minisign/minisign.key -m sha256sum.txt
