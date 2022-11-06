3dcdd14678b996c8b16210e6e01fc6a4a857f9ed
Alex Anderson <theheadlessserpentsec@protonmail.com>
2022-11-06 02:04:51 +0000

    Rotated public keys

--- make.sh
+++ make.sh
@@ -15,19 +15,18 @@ if [[ "$1" == "" ]]; then
 	for f in ./*; do
 		echo "Signing: $f"
 		# verify with GPG
-		gpg --armor --detach-sign --sign "$f"
+		gpg --default-key 83A6CF9EF57AC25B5C7F5D29285E6048A12321B2 --armor --detach-sign --sign "$f"
 		# verify with `minisign -Vm <file> -P RWQ0WYJ07DUokK8V/6LNJ9bf/O/QM9k4FSlDmzgEeXm7lEpw3ecYjXDM`
 		yes '' | minisign -S -s /home/user/.minisign/minisign.key -m "$f"
 	done
 	cd ../
-	cp /home/user/KEY_ROTATION.md.7DFFD7471FB76E2A8ABBBCDDD769B3749E933B8A.asc ./KEY_ROTATION.md.asc
+	cp /home/user/KEY_ROTATION.md.42FF35DB9DE7C088AB0FD4A70C216A52F6DF4920.asc ./KEY_ROTATION.md.asc
 	cp /home/user/KEY_ROTATION.md.902835EC74825934.minisig ./KEY_ROTATION.md.minisig
-	gpg --armor --export 42FF35DB9DE7C088AB0FD4A70C216A52F6DF4920 > 42FF35DB9DE7C088AB0FD4A70C216A52F6DF4920.asc
 	sha256sum *.md > sha256sum.txt
-	gpg --armor --detach-sign sha256sum.txt
+	gpg --default-key 83A6CF9EF57AC25B5C7F5D29285E6048A12321B2 --armor --detach-sign sha256sum.txt
 	yes '' | minisign -S -s /home/user/.minisign/minisign.key -m sha256sum.txt
 	b2sum *.md > b2sum.txt
-	gpg --armor --detach-sign b2sum.txt
+	gpg --default-key 83A6CF9EF57AC25B5C7F5D29285E6048A12321B2 --armor --detach-sign b2sum.txt
 	yes '' | minisign -S -s /home/user/.minisign/minisign.key -m b2sum.txt
 	echo "Signed all files."
 	echo "Done."
